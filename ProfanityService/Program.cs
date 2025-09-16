3d1e9a1def1415dcf5ffedc21d9c636e2aa1afc0
Daniel R. <danielrudcallesen96@gmail.com>
2025-09-16 10:38:21 +0200

    Added Connectionstring

--- ProfanityService/Program.cs
+++ ProfanityService/Program.cs
@@ -1,10 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using ProfanityService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ProfanityDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ProfanityDatabase")));
 
 var app = builder.Build();
 
