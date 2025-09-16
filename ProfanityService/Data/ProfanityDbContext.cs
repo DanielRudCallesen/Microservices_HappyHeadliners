4604e8d6af827e7154fc7b2b7c23b1097c0a9c28
Daniel R. <danielrudcallesen96@gmail.com>
2025-09-16 10:16:55 +0200

    Added DbContext
    + Nugetpackage EntityFrameworkCore.Relational

---
+++ ProfanityService/Data/ProfanityDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using ProfanityService.Models;
+
+namespace ProfanityService.Data
+{
+    public class ProfanityDbContext(DbContextOptions<ProfanityDbContext> options) : DbContext(options)
+    {
+        public DbSet<ProfanityWord> Words => Set<ProfanityWord>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProfanityWord>(b =>
+            {
+                b.ToTable("ProfanityWords");
+                b.HasKey(x => x.Id);
+                b.Property(x => x.Word).IsRequired().HasMaxLength(128);
+                b.HasIndex(x => x.Word).IsUnique();
+            });
+        }
+    }
+}
--- ProfanityService/obj/Debug/net8.0/ProfanityService.AssemblyInfo.cs
+++ ProfanityService/obj/Debug/net8.0/ProfanityService.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProfanityService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3e772bae674765de8fa1eff96f74ac01385a138")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f94b3dfdf091eb4f39dac1e991e9dc88ad148b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProfanityService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProfanityService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
