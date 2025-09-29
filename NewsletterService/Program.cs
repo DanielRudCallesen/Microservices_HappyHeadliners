b614ca3f42f233936abfa70cb3e1c93678e97258
Daniel R. <danielrudcallesen96@gmail.com>
2025-09-29 14:37:46 +0200

    Added the services needed

--- NewsletterService/Program.cs
+++ NewsletterService/Program.cs
@@ -1,11 +1,28 @@
+using Shared.Observability;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.AddObservability("NewsletterService");
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IImmediateArticleStore, InMemoryImmediateArticleStore>();
+
+builder.Services.AddHostedService<ArticleQueueSubscriber>();
+
+builder.Services.AddHttpClient("ArticleService", (sp, client) =>
+{
+    var cfg = sp.GetRequiredService<IConfiguration>();
+    var baseUrl = cfg["ArticleService:BaseUrl"] ?? "http://articleservice";
+    client.BaseAddress = new Uri(baseUrl.TrimEnd('/') + "/");
+});
+
+builder.Services.AddHostedService<DailyNewsletterWorker>();
+
 
 var app = builder.Build();
 
@@ -17,6 +34,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseObservabilityRequestEnrichment();
 
 app.UseAuthorization();
 
