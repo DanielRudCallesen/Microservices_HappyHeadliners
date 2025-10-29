480fc8aa7f69bbbe6cdcb6a2dec6dac9bce93ed2
Daniel R. <danielrudcallesen96@gmail.com>
2025-10-29 13:26:18 +0100

    Model & Interface for SubscriberQueue

---
+++ Shared.Messaging/SubscriberQueue/Model/NewSubscriber.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Messaging.SubscriberQueue.Model;
+
+public sealed record NewSubscriber(string Email, string? Name, DateTimeOffset SubscribedAt);
+
+
--- Shared.Messaging/obj/Debug/net8.0/Shared.Messaging.AssemblyInfo.cs
+++ Shared.Messaging/obj/Debug/net8.0/Shared.Messaging.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared.Messaging")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20489ad93112e53aac0e4aa5819d91bd859065c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7b4934613ed8f84ffbbe42b0fe3d57ab1f13df5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared.Messaging")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared.Messaging")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
